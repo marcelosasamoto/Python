f12ec1addda56a64c1ddada11a442b6ec804c284
marcelosasamoto <30844358+marcelosasamoto@users.noreply.github.com>
2020-02-27 10:05:29 -0300

    Update abstract.py

--- abstract.py
+++ abstract.py
@@ -1,18 +1,24 @@
 from abc import ABC, abstractmethod 
 import random, os
-  
+
+# referencia em https://www.geeksforgeeks.org/abstract-classes-in-python/
+#OBS programa sem tratamento de erro, portanto nao zoa a entrada.
+#classe pai
 class Abstrata(ABC): 
     def __init__(self, A, B):
+        for i in range(0, len(A)): #converte os elementos da lista para inteiro ex: ["1","2"] para [1,2]
+            A[i] = int (A[i])
+        for i in range(len(B)):     #aqui tambem
+            B[i] = int (B[i])
         self.A = A
         self.B = B
-        self.resultado = ""
-    def operacoes(self):
+    def operacoes(self):    # metodo abstato
         pass
 
-
+#classe filha
 class Uniao(Abstrata):
-    def operacoes(self):
-        A = set(self.A)
+    def operacoes(self):    # sobrescrita do metodo
+        A = set(self.A)     # o self.A herda da classe Abstrata
         B = set(self.B)
         C = A | B
         return sorted(C)
@@ -59,7 +65,7 @@ class Insere(Abstrata):
         elif y == "2":
             self.B.append(x)
             print("Valor adicionado com sucesso!")
-class Remove(Abstrata):
+class Remove(Abstrata):                             # sem tratamento de erro, se x nao estiver self.A ou self.B
     def operacoes(self,x,y):
         if y == "1":
             index = self.A.index(x)
@@ -103,9 +109,9 @@ if __name__ == '__main__':
     while(input_user != 3):
         input_user = int(input("Menu\n1)Informar valores da Array\n2)Visualizar valores\n3)Sair\n\nMenu de Operações\n4)União\n5)Interseção\n6)Diferença\n7)Membro\n8)Esvaziar conjunto\n9)Inserir\n10)Remover\n11)Atribuir\n12)Mínimo\n13)Máximo\n14)Igual\nOpção --> "))
         if(input_user == 1):
-            array_one_1 = input("Digite os números para a primeira Array -> ")
+            array_one_1 = input("Digite os números para a primeira Array separado por espaço -> ")
             array_one = array_one_1.split(" ")
-            array_two_1 = input("Digite os números para a segunda Array -> ")
+            array_two_1 = input("Digite os números para a segunda Array separado por espaço -> ")
             array_two = array_two_1.split(" ")
             os.system("clear")
         if(input_user == 2):
@@ -125,18 +131,18 @@ if __name__ == '__main__':
             print(Diferença(array_one,array_two).operacoes())
         if(input_user == 7):
             os.system("clear")
-            user = input("Digite o número desejado --> ")
+            user = int(input("Digite o número desejado --> "))
             print("Resultado -> ",Membro(array_one,array_two).operacoes(user,input("Digite qual conjunto. 1 ou 2")))
         if(input_user == 8):    
             os.system("clear")
             Cria_Conj_Vazio(array_one,array_two).operacoes(input("Digite qual conjunto. 1 ou 2"))
         if(input_user == 9):
             os.system("clear")
-            user_a = input("Digite o valor a ser inserido --> ")
+            user_a = int(input("Digite o valor a ser inserido --> "))
             Insere(array_one,array_two).operacoes(user_a,input("Digite qual conjunto. 1 ou 2"))
         if(input_user == 10):
             os.system("clear")
-            user_b = input("Digite o valor a ser removido --> ")
+            user_b = int(input("Digite o valor a ser removido --> "))
             Remove(array_one,array_two).operacoes(user_b,input("Digite qual conjunto. 1 ou 2"))
         if(input_user == 11):
             os.system("clear")
